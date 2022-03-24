3217d0a11f8aac87e37a4dbb6e3a6f37c4e2f08e
GithinOommen <90901501+GithinOommen@users.noreply.github.com>
2022-03-24 17:50:44 +0530

    Update ranges.c

--- ranges.c
+++ ranges.c
@@ -1,7 +1,7 @@
 #include "ranges.h"
 
 int Samples[8] = {3, 3, 5, 4, 10, 11, 12, 13};
-  
+  int NoOfSamples=sizeof(Samples)/sizeof(Samples[0]);
   int CountSamples()
   {
           int NoOfSamples = 0;
@@ -14,8 +14,8 @@ int Samples[8] = {3, 3, 5, 4, 10, 11, 12, 13};
   int checkRange(int LowLimit, int HighLimit)
   {
           int i=0,count=0;
-          int samplesno=CountSamples();
-   for(i=0;i<samplesno;++i)
+        //  int samplesno=CountSamples();
+   for(i=0;i<NoOfSamples;++i)
    {
           if((LowLimit<=Samples[i])&& (HighLimit>=Samples[i]))
           {
