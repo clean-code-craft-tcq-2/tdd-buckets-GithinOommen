68fc1ddd61db66607afbb0a231cc44268355b9ea
GithinOommen <90901501+GithinOommen@users.noreply.github.com>
2022-03-25 10:03:01 +0530

    Update ranges.c

--- ranges.c
+++ ranges.c
@@ -1,20 +1,20 @@
 #include "ranges.h"
 
 int Samples[8] = {3, 3, 5, 4, 10, 11, 12, 13};
-  int NoOfSamples=sizeof(Samples)/sizeof(Samples[0]);
- /* int CountSamples()
+//  int NoOfSamples=sizeof(Samples)/sizeof(Samples[0]);
+  int CountSamples()
   {
           int NoOfSamples = 0;
           NoOfSamples=sizeof(Samples)/sizeof(Samples[0]);
           
           return NoOfSamples;
   
-  }*/
+  }
   
   int checkRange(int LowLimit, int HighLimit)
   {
           int i=0,count=0;
-  //        int samplesno=CountSamples();
+          int samplesno=CountSamples();
    while(i<samplesno)
    {
          /* if((LowLimit<=Samples[i])&& (HighLimit>=Samples[i]))
