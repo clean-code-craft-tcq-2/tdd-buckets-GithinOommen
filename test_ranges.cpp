4bc82416e35c99acea68aa649f5f67c5240ec2f7
GithinOommen <90901501+GithinOommen@users.noreply.github.com>
2022-03-25 11:23:41 +0530

    Update test_ranges.cpp

--- test_ranges.cpp
+++ test_ranges.cpp
@@ -6,8 +6,10 @@
 //int feedback=0;
 TEST_CASE("Read No of samples from 1-5 range")
 {
-
+int Samples[8] = {3, 3, 5, 4, 10, 11, 12, 13};
+int NoOfSamples=sizeof(Samples)/sizeof(Samples[0]);
+  
  //feedback=checkRange(1,5);
- REQUIRE(checkRange(1, 5) == 4);
- REQUIRE(checkRange(1, 2) == 0);
+ REQUIRE(checkRange(1, 5,Samples) == 4);
+ REQUIRE(checkRange(1, 2,Samples) == 0);
 }
