ab69cab90e16aa7e8d2e4ef792936977f90177ee
GithinOommen <90901501+GithinOommen@users.noreply.github.com>
2022-03-25 11:34:16 +0530

    Update test_ranges.cpp

--- test_ranges.cpp
+++ test_ranges.cpp
@@ -12,4 +12,10 @@ int NoOfSamples=sizeof(Samples)/sizeof(Samples[0]);
  //feedback=checkRange(1,5);
  REQUIRE(checkRange(1, 5,NoOfSamples,Samples) == 4);
  REQUIRE(checkRange(1, 2,NoOfSamples,Samples) == 0);
+  
+int Samples1[0] = {};
+int NoOfSamples=sizeof(Samples)/sizeof(Samples[0]);
+  
+ //feedback=checkRange(1,5);
+ REQUIRE(checkRange(1, 5,NoOfSamples,Samples) == 0);
 }
