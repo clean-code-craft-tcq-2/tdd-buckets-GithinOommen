066a44c84b7e27c6e47009d667926b7d0fbf12d2
GithinOommen <90901501+GithinOommen@users.noreply.github.com>
2022-03-25 12:05:59 +0530

    Update test_ranges.cpp

--- test_ranges.cpp
+++ test_ranges.cpp
@@ -12,6 +12,7 @@ int NoOfSamples=sizeof(Samples)/sizeof(Samples[0]);
  //feedback=checkRange(1,5);
  REQUIRE(checkRange(1, 5,NoOfSamples,Samples) == 4);
  REQUIRE(checkRange(1, 2,NoOfSamples,Samples) == 0);
+  REQUIRE(checkRangelimit(1,3,0,Samples)==1);
   
 int Samples1[0] = {};
 int NoOfSamples1=sizeof(Samples1)/sizeof(Samples1[0]);
