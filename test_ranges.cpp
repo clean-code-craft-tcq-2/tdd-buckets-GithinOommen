8414c59c192999e1a15afae141c904d7a51f9158
GithinOommen <90901501+GithinOommen@users.noreply.github.com>
2022-04-03 14:07:04 +0530

    Update test_ranges.cpp

--- test_ranges.cpp
+++ test_ranges.cpp
@@ -21,3 +21,17 @@ int NoOfSamples1=sizeof(Samples1)/sizeof(Samples1[0]);
  //feedback=checkRange(1,5);
  REQUIRE(checkRange(1, 5,NoOfSamples1,Samples1) == 0);
 }
+TEST_CASE("Writing integer of array with 12bit Sensor Value and compare with ADC result")
+{
+   int size =8;
+  int resolution =12;
+   int ActualOutput[10];
+   int SensorValue[8] = {300 , 500 , 1400 , 3000 , 2500 , 3600 , 4000,4094};
+   int ExpectedOutput[8] = {1 ,1 , 3, 7 , 6 , 9 ,10 ,10 };
+  GetADCValue(SensorValue,ActualOutput,size,resolution);
+for(int index=0; size > index ; index++)
+   {
+         printf("\nActualOutput[%d],%d",index,ActualOutput[index]);
+     REQUIRE(ActualOutput[index] == ExpectedOutput[index]);
+   }
+}
