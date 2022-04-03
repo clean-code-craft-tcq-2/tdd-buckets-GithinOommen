f1af4330d0045c15f135205bffdfa6386607025f
GithinOommen <90901501+GithinOommen@users.noreply.github.com>
2022-04-03 14:02:26 +0530

    Update ranges.h

--- ranges.h
+++ ranges.h
@@ -1,6 +1,10 @@
 #ifndef RANGES_H
 #define RANGES_H
 #include <stdio.h>
+#include<math.h>
+#include "adc.h"
+#include <stdlib.h>
+#include <float.h>
 
 
  int checkRange(int LowLimit, int HighLimit,int samplesno, int* Samples);
